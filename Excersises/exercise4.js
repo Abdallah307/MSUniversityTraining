afb72cee02e81f1bbe100f5ba468058bc919f662
Sahar Abdelhaq <saharabdelhaq02@gmail.com>
2024-04-17 07:32:34 +0200

    JavaScript program to find the area of a triangle

--- Excersises/exercise4.js
+++ Excersises/exercise4.js
@@ -1,4 +1,14 @@
 /*
   Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7. 
 */
-const triangleArea = (pra1, pra2, pra3) => {};
+const a = 5;
+const b = 6;
+const c = 7;
+
+const triangleAreaCalculator = (a, b, c) => {
+  let s = 0.5 * (a + b + c);
+  let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  console.log("The area of the triangle: " + area);
+};
+
+triangleAreaCalculator(a, b, c);
