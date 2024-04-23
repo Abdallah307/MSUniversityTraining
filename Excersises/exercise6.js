a76cb782d0c0e8c557bf5ce92b239e5e75cf6f95
Sahar Abdelhaq <saharabdelhaq02@gmail.com>
2024-04-23 20:33:10 +0300

    resolve the comments

--- Excersises/exercise6.js
+++ Excersises/exercise6.js
@@ -1,15 +1,13 @@
 /* Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards).*/
 
 const checkPalindrome = (str) => {
-  let righToleft = "";
-  let leftToRight = str;
+  let reveredStr = "";
 
   for (let i = str.length; i >= 0; i--) {
-    righToleft += str.charAt(i);
+    reveredStr += str.charAt(i);
   }
-  console.log(leftToRight);
-  console.log(righToleft);
-  if (leftToRight === righToleft) console.log("palindrome");
+  console.log(reveredStr);
+  if (str === reveredStr) console.log("palindrome");
   else {
     console.log("Not palindrome");
   }
