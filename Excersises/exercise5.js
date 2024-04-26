6fff309031655a54a4f474a1e57dfa6aef25b5be
Sahar Abdelhaq <saharabdelhaq02@gmail.com>
2024-04-27 00:02:10 +0300

    modify the varible name

--- Excersises/exercise5.js
+++ Excersises/exercise5.js
@@ -5,15 +5,15 @@ Write a JavaScript program to find the maximum number in an array.
 const numbers = [1, 40, 9, 33];
 
 const getMaxNumber = (numbers) => {
-  let temp = numbers[0];
+  let max = numbers[0];
 
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > temp) {
-      temp = numbers[i];
+    if (numbers[i] > max) {
+      max = numbers[i];
       //   console.log(numbers[i]);
     }
   }
-  console.log("maximum numbes is: " + temp);
+  console.log("maximum numbes is: " + max);
 };
 
 getMaxNumber(numbers);
